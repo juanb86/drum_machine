124bd173edac861ec8a961a9ea9a2f7f33a72d75
juanb86 <juan.e.begnis@gmail.com>
2020-06-29 04:48:20 -0300

    Implement ref to audio element

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Q from "./cosmo_short_Bmaj9.wav";
+
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+    this.audio = React.createRef();
+    this.playClip = this.playClip.bind(this);
+  }
+
+  playClip() {
+    this.audio.current.play();
+  }
+
+  render() {
+    const id = "Q";
+    return (
+      <div className="drum-pad" id={id} onClick={this.playClip}>
+        Q{" "}
+        <audio ref={this.audio} className="clip" id={id}>
+          <source src={Q} />
+        </audio>
+      </div>
+    );
+  }
+}
 
 class Machine extends React.Component {
   constructor(props) {
@@ -9,15 +37,12 @@ class Machine extends React.Component {
       asd: "asd",
     };
   }
+
   render() {
     return (
       <div id="drum-machine">
         <div id="display"></div>
-        <div>
-          <div className="drum-pad" id="kick" onClick="">
-            Q<audio className="clip" id="Q" src="" ></audio>
-          </div>
-        </div>
+        <DrumPad />
       </div>
     );
   }
