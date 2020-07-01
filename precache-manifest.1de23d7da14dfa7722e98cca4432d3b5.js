ac5734837cd54f6d7cb6530d2a7268b524d782b2
juanb86 <juan.e.begnis@gmail.com>
2020-06-30 21:23:49 -0300

    Updates

---
+++ precache-manifest.1de23d7da14dfa7722e98cca4432d3b5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42446d3707dea29fa140334b82485d7d",
+    "url": "/drum_machine/index.html"
+  },
+  {
+    "revision": "66c29db85f6c8b3fc4d7",
+    "url": "/drum_machine/static/css/main.4555c3f2.chunk.css"
+  },
+  {
+    "revision": "bcf1d1a5d30d30152b1d",
+    "url": "/drum_machine/static/js/2.7dbf16a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum_machine/static/js/2.7dbf16a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c29db85f6c8b3fc4d7",
+    "url": "/drum_machine/static/js/main.2d828339.chunk.js"
+  },
+  {
+    "revision": "d129b22bb7a1283ece0c",
+    "url": "/drum_machine/static/js/runtime-main.d5a7a7e7.js"
+  },
+  {
+    "revision": "36f0247c831785c62abe270b105985f5",
+    "url": "/drum_machine/static/media/HH.36f0247c.wav"
+  },
+  {
+    "revision": "ade232e5122dbbd0807f7554434f9dc5",
+    "url": "/drum_machine/static/media/HH_Big.ade232e5.wav"
+  },
+  {
+    "revision": "857a10e525e16acbd400895d2acd4e73",
+    "url": "/drum_machine/static/media/HH_small.857a10e5.wav"
+  },
+  {
+    "revision": "2a9d044e40f808899452d58c9c80ea9e",
+    "url": "/drum_machine/static/media/Kick_drum1.2a9d044e.wav"
+  },
+  {
+    "revision": "3fd5d6c6b3e2eaa75d2bd8fd0cb3f848",
+    "url": "/drum_machine/static/media/Kick_drum2.3fd5d6c6.wav"
+  },
+  {
+    "revision": "df8cd9f5569883a1a46a1706e985e18d",
+    "url": "/drum_machine/static/media/Kick_drum3.df8cd9f5.wav"
+  },
+  {
+    "revision": "dc903e848c6417171a9116f718ea96bd",
+    "url": "/drum_machine/static/media/Snare1.dc903e84.wav"
+  },
+  {
+    "revision": "067d43564d7b578d1e239c865f63719d",
+    "url": "/drum_machine/static/media/Snare2.067d4356.wav"
+  },
+  {
+    "revision": "4971b6e223de95bf9d6e7e6774e83597",
+    "url": "/drum_machine/static/media/Snare3.4971b6e2.wav"
+  }
+]);
